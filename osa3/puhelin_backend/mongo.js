0dc17fb9fbbdba3ef3106bc24724f4cd9ae4b541
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-02-11 14:58:30 +0200

    osa3 valmis

--- osa3/puhelin_backend/mongo.js
+++ osa3/puhelin_backend/mongo.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 console.log(process.argv)
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+	console.log('give password as argument')
+	process.exit(1)
 }
 
 
@@ -17,29 +17,31 @@ const url = `mongodb+srv://juhkarhu:${password}@cluster0.95rah.mongodb.net/phone
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+	name: String,
+	number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length == 3) {
-    Person.find({}).then(result => {
-        console.log(result)
-        mongoose.connection.close()
-    })
-} else if (process.argv.length == 5) {
+if (process.argv.length === 3) {
+	Person.find({}).then(result => {
+		console.log(result)
+		mongoose.connection.close()
+	})
+} else if (process.argv.length === 5) {
 
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
+	const person = new Person({
+		name: process.argv[3],
+		number: process.argv[4]
+	})
 
-    person.save().then(response => {
-        console.log(`Added person ${person.name} with a number of ${person.number} to phonebook`)
-        mongoose.connection.close()
-    })
+
+	//person.save().then(response => {
+	person.save().then(() => {
+		console.log(`Added person ${person.name} with a number of ${person.number} to phonebook`)
+		mongoose.connection.close()
+	})
 
 } else {
-    mongoose.connection.close()
+	mongoose.connection.close()
 }
