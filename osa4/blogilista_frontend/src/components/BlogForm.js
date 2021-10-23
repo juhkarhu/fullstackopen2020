7b6ef766eb2e22e3060822e1b717570004bbee04
Juhana Karhunen <juhana.karhunen@gmail.com>
2021-10-23 09:04:55 +0300

    updates

--- osa4/blogilista_frontend/src/components/BlogForm.js
+++ osa4/blogilista_frontend/src/components/BlogForm.js
@@ -1,24 +1,62 @@
 import React, { useState } from 'react'
 
-const BlogForm = ({ onSubmit, handleChange, value }) => {
 
-  
 
-  return (
-    <div>
-      <h2>Create a new blog</h2>
 
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={handleChange}
-        />
-        <button type='submit'>save</button>
-      </form>
-    </div>
-  )
+
+const AddBlogForm = ({ createBlog }) => {
+
+    const [newTitle, setNewTitle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
+    const [newUrl, setNewUrl] = useState('')
+
+    const handleTitleChange = (event) => {
+        event.preventDefault()
+        setNewTitle(event.target.value)
+    }
+
+    const handleAuthorChange = (event) => {
+        event.preventDefault()
+        setNewAuthor(event.target.value)
+    }
+
+    const handleUrlChange = (event) => {
+        event.preventDefault()
+        setNewUrl(event.target.value)
+    }
+
+
+    const addBlog = (event) => {
+        event.preventDefault()
+        createBlog({
+            title: newTitle,
+            author: newAuthor,
+            url: newUrl
+        })
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+    }
+
+
+    return (
+        <form onSubmit={addBlog}>
+            <div>
+                Title: <input value={newTitle} onChange={handleTitleChange} />
+            </div>
+            <div>
+                Author: <input value={newAuthor} onChange={handleAuthorChange} />
+            </div>
+            <div>
+                Url: <input value={newUrl} onChange={handleUrlChange} />
+            </div>
+            <div>
+                <button type="submit">Add</button>
+            </div>
+        </form>
+    )
 
 }
 
 
-export default BlogForm
+export default AddBlogForm
